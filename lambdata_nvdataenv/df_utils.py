f5b1b64230655385ad500ad872eacc71b8c61483
Navo Visagan <navo17@gmail.com>
2019-10-01 09:45:06 -0700

    Display Options & Null ifon

--- lambdata_nvdataenv/df_utils.py
+++ lambdata_nvdataenv/df_utils.py
@@ -2,6 +2,23 @@
 Utility Functions for working with DataFrames
 """
 
-import pandas
+import pandas as pd
+import numpy as np
 
-TEST_DF = pandas.DataFrame([1,2,3])
+TEST_DF = pd.DataFrame([1,2,3])
+
+# Function to set notebook display options
+def optimaloptions(df):
+    with pd.set_option('display.max_rows', 999):
+        with pd.set_option('precision', 5):
+            print(df)
+
+
+def nullinfo(df):
+	no_of_rows = df.shape[0]  #Get number of rows in the Dataframe
+	no_of_nulls = df.isnull().sum() #Get a dataframe containing number of nulls in each columns
+	null_notify = pd.DataFrame(no_of_nulls).reset_index() # Creating a Dataframe with Null values information
+	null_notify.columns = ['Column Name','No of Nulls'] #Naming the columns of the DataFrame
+	null_notify['Nulls percent']=np.round(null_notify['No of Nulls']/no_of_rows,2)#using round function to have only 2 decimal points
+#Return to the Dataframe
+	return null_notify
